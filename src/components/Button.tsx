1902146916373e66ead856c9653b1814107071ea
claudia.tss <claudia.tss@outlook.com>
2023-05-26 12:32:39 -0300

    Preparado para receber API

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,17 +1,17 @@
 type ButtonProps={
     type?:"submit" | "button" | "reset";
-    children: string;
-    onClick?: ()=>void;
+    children: React.ReactNode;
+    onClick?: ()=>number;
 }
 
-export function Button(props:ButtonProps){
+export function Button({type, children, onClick}:ButtonProps){
     return (
       <button 
-      type={props.type}
-      onClick={props.onClick}
+      type={type}
+      onClick={onClick}
         className="bg-blue-400 text-slate-50 font-bold uppercase py-1 px-3 mx-2 rounded-md"
       >
-        {props.children}
+        {children}
       </button>
     );
 }
--- src/routes/CreateNotepadRoute.tsx
+++ src/routes/CreateNotepadRoute.tsx
@@ -1,13 +1,63 @@
+import {useState} from "react"
 import { Button } from "../components/Button"
 
 export function CreateNotepadRoute() {
+    const [title, setTitle] = useState("")
+    const [subtitle, setSubtitle]=useState("")
+    const [content, setContent]=useState("")
+
+
   return (
     <div>
-      <form className="flex flex-col items-center gap-10">
-        <h1 className="font-bold uppercase mt-10 font-mono text-2xl">Criar posts</h1>
-        <input className="w-96 p-1" type="text" placeholder="Digite o título" />
-        <input className="w-96 p-1" type="text" placeholder="Digite o subtítulo" />
-        <textarea className="w-96 h-48 p-1" placeholder="Digite o conteúdo" />
+      <form
+        className="flex flex-col gap-4 md:max-w-screen-md md:mx-auto"
+        onSubmit={(event) => {
+          event.preventDefault();
+
+          // implementar logica de envio do form
+
+          setTitle(" ");
+          setSubtitle(" ");
+          setContent(" ");
+        }}
+      >
+        <h1 className="flex justify-center font-bold uppercase mt-10 font-mono text-2xl">
+          Criar posts
+        </h1>
+
+        <input
+          className="px-2 py-1 rounded-md border focus:border-blue-500 outline-none"
+          value={title}
+          onChange={(event) => {
+            const novoTitle = event.target.value;
+            setTitle(novoTitle);
+          }}
+          type="text"
+          placeholder="Digite o título"
+        />
+
+        <input
+          className="px-2 py-1 rounded-md border focus:border-blue-500 outline-none"
+          value={subtitle}
+          onChange={(event) => {
+            const novoSubtitle = event.target.value;
+            setSubtitle(novoSubtitle);
+          }}
+          type="text"
+          placeholder="Digite o subtítulo"
+        />
+
+        <textarea
+          className="px-2 py-1 rounded-md border focus:border-blue-500 outline-none"
+          value={content}
+          rows={4}
+          onChange={(event) => {
+            const novoContent = event.target.value;
+            setContent(novoContent);
+          }}
+          placeholder="Digite o conteúdo"
+        />
+
         <Button type="submit">Enviar</Button>
       </form>
     </div>
